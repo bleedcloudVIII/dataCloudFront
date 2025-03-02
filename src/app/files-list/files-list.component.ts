03d53697d7e063f301813cbc31e803a006eab1e3
bleedcloudVIII <yurikaras8@gmail.com>
2025-03-02 17:30:34 +1000

    Сделал панель с файлами, как отдельный компонент + дизайн

---
+++ src/app/files-list/files-list.component.ts
@@ -0,0 +1,19 @@
+import {Component, Input} from '@angular/core';
+import {MatTab} from '@angular/material/tabs';
+import {NgForOf} from '@angular/common';
+
+@Component({
+  selector: 'app-files-list',
+  imports: [
+    NgForOf
+  ],
+  templateUrl: './files-list.component.html',
+  styleUrl: './files-list.component.scss'
+})
+export class FilesListComponent {
+  @Input() files!: string[];
+
+  constructor() {
+
+  }
+}
