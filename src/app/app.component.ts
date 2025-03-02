60d59da810e9381d545b7c9a46d578aaff856273
bleedcloudVIII <yurikaras8@gmail.com>
2025-03-02 17:30:46 +1000

    Сделал панель с файлами, как отдельный компонент + дизайн

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,20 @@
 import { Component } from '@angular/core';
 import {MatTab, MatTabGroup} from '@angular/material/tabs';
 import {NgForOf} from '@angular/common';
+import {FilesListComponent} from './files-list/files-list.component';
 
 @Component({
   selector: 'app-root',
   imports: [
     MatTabGroup,
     MatTab,
-    NgForOf
+    NgForOf,
+    FilesListComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  tabs = ['location', 'location1', 'location2', 'location', 'location1', 'location2'];
-
+  tabs = ['Общее пространство', 'Пространство1', 'Моё', 'Лето2020', 'Анапа'];
+  files = [['asd.txt', 'dadte.txt', 'file1.txt', 'file2.txt', 'asd.txt', 'd222222222222222222adte.txt', 'file1.txt', 'file2.txt', 'asd.txt', 'dadte.txt', 'file1.txt', 'file2.txt'], ['file1.txt', 'file2.txt'],];
 }
