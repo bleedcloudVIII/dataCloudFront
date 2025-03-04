355d9c123a6f7799ccc71a0e8486a39f2a88fddc
bleedcloudVIII <yurikaras8@gmail.com>
2025-03-04 11:17:52 +1000

    фывф

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import {MatTab, MatTabGroup} from '@angular/material/tabs';
 import {NgForOf} from '@angular/common';
 import {FilesListComponent} from './files-list/files-list.component';
+import { RegistrationComponent } from './registration/registration.component';
+import {MatDialog, MatDialogRef} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
@@ -17,4 +19,17 @@ import {FilesListComponent} from './files-list/files-list.component';
 export class AppComponent {
   tabs = ['Общее пространство', 'Пространство1', 'Моё', 'Лето2020', 'Анапа'];
   files = [['asd.txt', 'dadte.txt', 'file1.txt', 'file2.txt', 'asd.txt', 'd222222222222222222adte.txt', 'file1.txt', 'file2.txt', 'asd.txt', 'dadte.txt', 'file1.txt', 'file2.txt'], ['file1.txt', 'file2.txt'],];
+
+  constructor(private dialog: MatDialog) {
+  }
+
+  onAccountClick() {
+    const dialogRef = this.dialog.open(RegistrationComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        // do something
+      }
+    });
+  }
 }
--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {MatDialogRef} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-registration',
@@ -7,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './registration.component.scss'
 })
 export class RegistrationComponent {
+  constructor(public dialogRef: MatDialogRef<RegistrationComponent>) {
+  }
 
 }
