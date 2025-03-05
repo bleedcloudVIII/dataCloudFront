31aae05acd8d928d900e464b4f548e1e44743a6d
bleedcloudVIII <yurikaras8@gmail.com>
2025-03-05 11:26:27 +1000

    Выбор файла

--- src/app/files-list/files-list.component.ts
+++ src/app/files-list/files-list.component.ts
@@ -15,13 +15,28 @@ export class FilesListComponent {
   constructor() {
   }
 
-  onAddClick() {
-    console.log('adas');
-  }
-
   onFileClick() {
     // TODO NE RABOTAET
     console.log('a');
   }
 
+  onFileSelected(event: any) {
+    const input = event.target as HTMLInputElement;
+
+    console.log('try...');
+
+    // Проверяем, есть ли выбранные файлы
+    if (input.files && input.files.length > 0) {
+      const file = input.files[0]; // Получаем первый выбранный файл
+
+      // Выводим информацию о файле
+      console.log('Имя файла:', file.name);
+      console.log('Размер файла:', file.size, 'байт');
+      console.log('Тип файла:', file.type);
+
+      // Если нужно получить путь к файлу (неполный путь, только имя файла)
+      console.log('Имя файла (путь):', file.webkitRelativePath);
+
+    }
+  }
 }
